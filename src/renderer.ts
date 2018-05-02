282e691ba307f2fb9fe9fa2b4cc5d6d68f316d46
Pavol Hejny <me@pavolhejny.com>
2018-05-03 01:32:30 +0200

    async repos

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,15 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+import Workspace from "./classes/Workspace";
+
+const workspace = new Workspace('../../');
+
+
+
+async function main(){
+
+    console.log(workspace);
+    const repositories = await workspace.unassignedRepositoriesRoot();
+
+    console.log(repositories);
+}
+
+main();
