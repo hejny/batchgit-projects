8c4d74a6c6a7fd75fede1f9f8cf53e51da9ea246
Pavol Hejny <me@pavolhejny.com>
2018-07-06 12:02:45 +0200

    reapair

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ async function main(){
     console.log(repositories.join('\n'));
     console.log('---------------------Ignored:');
     console.log(repositoriesIgnored.join('\n'));
-    process.exit();
+    //process.exit();
 
 
 
